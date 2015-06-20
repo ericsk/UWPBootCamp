89d1526ae5ec55430cb8e4b805291304ba61099b
Eric ShangKuan <ericsk@gmail.com>
2015-06-21 00:37:37 +0800

    Add the HOL1 project files for reference.

---
+++ Projects/HOL1-CurrencyConverter/CurrencyConverter/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace CurrencyConverter
+{
+  using Windows.UI.Xaml;
+  using Windows.UI.Xaml.Controls;
+
+  public sealed partial class MainPage : Page
+  {
+    const double RATE = 31.0;
+
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private void OnConvertButtonClick(object sender, RoutedEventArgs e)
+    {
+      double usd;
+
+      if (double.TryParse(USD.Text, out usd))
+      {
+        TWD.Text = (usd * RATE).ToString();
+      }
+    }
+  }
+}
