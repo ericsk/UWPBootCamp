4cc3e2207cd205d420f3df698aa7ffbb009e13a7
Eric ShangKuan <ericsk@gmail.com>
2015-08-01 22:32:30 +0800

    Add BMI Sample

---
+++ Presentation/1_XAML_Controls/BMI Calculator/BMI Calculator/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BMI_Calculator
+{
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
+    {
+      this.InitializeComponent();
+    }
+
+    private void OnCalculateButtonClicked(object sender, RoutedEventArgs e)
+    {
+      double height, weight;
+
+      if (double.TryParse(Height.Text, out height) && double.TryParse(Weight.Text, out weight))
+      {
+        var heightInM = height / 100.0;
+
+        BMIResult.Text = (weight / (heightInM * heightInM)).ToString();
+      }
+    }
+  }
+}
